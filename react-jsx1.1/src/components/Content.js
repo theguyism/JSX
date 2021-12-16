0dccae2bf726d3441c3cc44521ed66f0c529dd46
theguyism <95016197+theguyism@users.noreply.github.com>
2021-12-16 14:27:35 +0700

    Add files via upload

---
+++ react-jsx1.1/src/components/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Content extends React.Component {
+    render () {
+        return <div>Hello React!</div>
+    }
+  }
+
+export default Content;
---
+++ react-jsx1.1/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+class Footer extends React.Component {
+    render () {
+        return <div>Hello React!</div>
+    }
+  }
+  
+
+
+
+
+
+export default Footer;
